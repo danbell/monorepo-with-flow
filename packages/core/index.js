a457378bfaab7375ab641a9338ab8fdae2407750
Daniel Bell <daniel.m.bell@gmail.com>
2018-05-21 09:22:21 +1000

    Added two simple packages.

---
+++ packages/core/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import { max, square } from '@lerna-prototype/math';
+
+console.log(max(3,4));
+
+console.log(square(6));
---
+++ packages/math/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+export function max(a: number, b: number): number {
+  return a > b ? a : b;
+}
+
+export function square(a: number): number {
+  return a * a;
+}
